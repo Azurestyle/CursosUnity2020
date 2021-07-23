5193448d54933518ff4183bd62f5e4caed3bbc15
Nacho <azurestyle@gmail.com>
2021-07-23 12:27:06 +0200

    Proyecto 5

--- 05_QuickClick/Assets/_Scripts/Target.cs
+++ 05_QuickClick/Assets/_Scripts/Target.cs
@@ -5,9 +5,43 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Rigidbody _rigidbody;
+
+    private float minForce = 12,
+        maxForce = 16,
+        maxTorque = 10,
+        xRange = 4,
+        ySpawnPos = -6;
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddForce(RandomForce(), ForceMode.Impulse);
+        _rigidbody.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(),ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+    }
+    /// <summary>
+    /// Genera un vector aleatorio en 30
+    /// </summary>
+    /// <returns>Fuerza aleatoria para arriba</returns>
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minForce, maxForce);
+    }
+    /// <summary>
+    /// Genera número aleatorio
+    /// </summary>
+    /// <returns>Valor aleatorio entre -maxTorque y maxTorque</returns>
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+    /// <summary>
+    /// Genera posición aleatoria
+    /// </summary>
+    /// <returns>Devuelve posición aleatoria en 3d, con la coordenada z =0</returns>
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 
     // Update is called once per frame
